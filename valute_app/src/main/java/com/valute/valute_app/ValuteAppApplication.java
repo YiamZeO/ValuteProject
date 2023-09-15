df5e257756147ffd60ab98c2f06da3a361b337d3
ZeO <102325825+YiamZeO@users.noreply.github.com>
2023-09-15 17:19:38 +0300

    start valute app

---
+++ valute_app/src/main/java/com/valute/valute_app/ValuteAppApplication.java
@@ -0,0 +1,13 @@
+package com.valute.valute_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuteAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValuteAppApplication.class, args);
+    }
+
+}
